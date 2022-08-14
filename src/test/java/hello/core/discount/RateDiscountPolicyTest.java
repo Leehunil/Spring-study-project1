065e87bbbb7f084e0627e598244ade7ad4de183b
Leehunil <hunil9978@gmail.com>
2022-08-14 17:48:18 +0900

    Second commit

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,40 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+
+    void VIP_O(){
+        //given
+        Member member= new Member(1L,"VIP customer", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member,10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아닌 고객은 10% 할인이 적용되지 않는다")
+
+    void VIP_x(){
+        //given
+        Member member = new Member(2L,"BASIC customer",Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member,10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+}
